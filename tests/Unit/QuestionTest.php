4057a788d27b2fe907ba8a753d000eb2ca8885fd
andycc77 <andycc77e@gmail.com>
2023-07-30 02:46:41 +0800

    refactory

--- tests/Unit/QuestionTest.php
+++ tests/Unit/QuestionTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use App\Models\Answer;
 use App\Models\Question;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -20,5 +21,19 @@ class QuestionTest extends TestCase
 
         $this->assertInstanceOf('Illuminate\Database\Eloquent\Relations\HasMany', $question->answers());
 
+    }
+
+    /** @test */
+    public function questions_with_published_at_date_are_published()
+    {
+        $publishedQuestion1 = Question::factory()->published()->create();
+        $publishedQuestion2 = Question::factory()->published()->create();
+        $unpublishedQuestion = Question::factory()->unpublished()->create();
+
+        $publishedQuestions = Question::published()->get();
+
+        $this->assertTrue($publishedQuestions->contains($publishedQuestion1));
+        $this->assertTrue($publishedQuestions->contains($publishedQuestion2));
+        $this->assertFalse($publishedQuestions->contains($unpublishedQuestion));
     }
 }
